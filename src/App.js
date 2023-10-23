95f34c9bdbeb479a3d79585c66ae323a0188537f
Alan Cyril <alancyril059@gmail.com>
2023-10-23 21:12:09 +0100

    Add Hourly and Daily forecast section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
       <Inputs />
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
